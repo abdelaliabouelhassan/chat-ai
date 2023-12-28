5b33670b92062a679425212d59241bb8e1b7abc7
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-12-28 20:21:44 +0100

    list tables and button add chat

--- components/chat/Contact.vue
+++ components/chat/Contact.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="{' hidden':!openContact,'block':openContact}"  class=" absolute sm:relative  top-0 left-0 bg-white dark:bg-gray-800 sm:bg-transparent  z-50 sm:block sm:max-w-[16rem] lg:max-w-sm w-full h-full border-r dark:border-gray-500 ">
-           <div class=" w-full flex justify-end p-4">
-         
+           <div class=" w-full flex justify-end px-4">
+            
               <button @click="openContact = !openContact" type="button" class="dark:border-gray-500 inline-flex sm:hidden items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -9,8 +9,17 @@
                </button> 
            </div>
               <div class=" w-full relative">
-                <div class="  absolute top-0 h-10 sm:h-[7.1rem] border-b-2 border-gray-200 dark:border-gray-500 left-0 w-full bg-white dark:bg-gray-800">
-                    <div class="flex items-center bg-white w-full  dark:bg-gray-800 absolute bottom-2 px-3 py-2  ">
+               
+                <div class="  absolute top-0 h-10 sm:h-[7.1rem] border-b-2 flex flex-col justify-between border-gray-200 dark:border-gray-500 left-0 w-full bg-white dark:bg-gray-800">
+                   <div class=" w-full px-4 pt-2">
+                     <button class=" w-full  bg-blue-500 text-white flex items-center hover:scale-x-105 duration-200 rounded-md justify-center py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                           <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        <span>Add new Chat</span>
+                    </button>
+                   </div>
+                    <div class="flex items-center bg-white w-full  dark:bg-gray-800   px-3 py-2  ">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 stroke-slate-300 mr-5" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor" stroke-width="3">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -39,7 +48,7 @@
                 
 
                     <div
-                        class="px-5 py-4   flex items-center bg-white dark:bg-gray-900 cursor-pointer border-l-4 border-l-blue-500  dark:border-y-gray-500 border-t border-b dark:hover:bg-gray-700">
+                        class="px-5 py-4   flex items-center bg-white dark:bg-gray-800 cursor-pointer border-l-4 border-l-blue-500  dark:border-y-gray-500 border-t border-b dark:hover:bg-gray-700">
                         <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500 "
                             class="h-12 w-12 border-2 border-white rounded-full" alt="">
                         <div class="ml-4">
--- components/chat/Messages.vue
+++ components/chat/Messages.vue
@@ -52,7 +52,7 @@
                         </div>
                     </div>
 
-                        <div class="chat-message">
+                   <div class="chat-message">
                         <div class="flex items-end">
 
                         <div class="flex items-start gap-2.5">
@@ -92,6 +92,102 @@
 
                         </div>
                     </div>
+
+                     <div class="chat-message">
+                        <div class="flex items-end">
+                            <div class="flex items-start gap-2.5">
+                                <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=14" alt="Jese image">
+                                <div class="flex flex-col w-full  space-y-4 leading-1.5 p-4 border-gray-200   bg-gray-300  rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                                    <div class="flex items-center space-x-2 rtl:space-x-reverse">
+                                        <span class="text-sm font-semibold text-gray-600 dark:text-white">Bonnie Green</span>
+                                        <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
+                                    </div>
+                                 
+
+                                    <div class="relative overflow-x-auto  sm:rounded-lg">
+                                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                                <tr>
+                                                    <th scope="col" class="px-6 py-3">
+                                                        Name
+                                                    </th>
+                                                    <th scope="col" class="px-6 py-3">
+                                                        Title
+                                                    </th>
+                                                    <th scope="col" class="px-6 py-3">
+                                                        Rate
+                                                    </th>
+                                                    <th scope="col" class="px-6 py-3">
+                                                        description
+                                                    </th>
+                                                    <th scope="col" class="px-6 py-3">
+                                                        Action
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        Hotel 1
+                                                    </th>
+                                                    <td class="px-6 py-4">
+                                                        Silver
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        10
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quis d
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                                                    </td>
+                                                </tr>
+                                                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        Hotel 2
+                                                    </th>
+                                                    <td class="px-6 py-4">
+                                                        Silver
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        8
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quis d
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                                                    </td>
+                                                </tr>
+                                                   <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        Hotel 3
+                                                    </th>
+                                                    <td class="px-6 py-4">
+                                                        Silver
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        9
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quis d
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                    <!-- <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span> -->
+                                </div>
+                                </div>
+                        </div>     
+                        </div>
+
+                    
 
 
                     
