a292df4a522af61d9084af8c45a906a49def4e2d
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-12-28 10:56:42 +0100

    fixes

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-   <!-- component -->
+  
         <div class=" w-full h-screen flex items-start">
-            <div class=" hidden sm:block sm:max-w-[16rem] lg:max-w-sm w-full h-full">
+            <div class=" hidden sm:block sm:max-w-[16rem] lg:max-w-sm w-full h-full border-r ">
               <div class=" w-full relative">
                 <div class="  absolute top-0 h-[7.1rem] border-b-2 border-gray-200 left-0 w-full bg-white">
                     <div class="flex items-center bg-white w-full  absolute bottom-2 px-3 py-2  ">
@@ -75,7 +75,7 @@
                         </div>
                     </div>
 
-                    <div v-for="n in 20" :key="n"
+                    <div 
                         class="px-5 py-4   flex items-center   cursor-pointer border-l-4 border-l-transparent hover:bg-slate-100">
                         <img src="https://images.unsplash.com/photo-1499887142886-791eca5918cd?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHVzZXJzfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500 "
                             class="h-12 w-12 border-2 border-white rounded-full" alt="">
@@ -111,8 +111,8 @@
                             <span class="text-lg text-gray-600">Junior Developer</span>
                         </div>
                     </div>
-                    <!-- <div class="flex items-center space-x-2">
-                        <button type="button" class="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
+                    <div class="flex items-center space-x-2">
+                        <!-- <button type="button" class="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                             </svg>
@@ -121,13 +121,18 @@
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                             </svg>
-                        </button>
+                        </button> -->
                         <button type="button" class="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
-                            </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                          </svg>
+                          <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                          </svg> -->
+
+
                         </button>
-                    </div> -->
+                    </div>
                 </div>
                 <div id="messages" class="flex h-full flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
                     
@@ -146,12 +151,12 @@
                         <div class="flex items-end">
                             <div class="flex items-start gap-2.5">
                                 <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=14" alt="Jese image">
-                                <div class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                                <div class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200   bg-gray-300  rounded-e-xl rounded-es-xl dark:bg-gray-700">
                                     <div class="flex items-center space-x-2 rtl:space-x-reverse">
-                                        <span class="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
+                                        <span class="text-sm font-semibold text-gray-600 dark:text-white">Bonnie Green</span>
                                         <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
                                     </div>
-                                    <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will really appreciate the improvements.</p>
+                                    <p class="text-sm font-normal py-2.5 text-gray-600 dark:text-white">That's awesome. I think our users will really appreciate the improvements.</p>
                                     <!-- <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span> -->
                                 </div>
                                 </div>
@@ -164,12 +169,12 @@
                         <div class="flex items-start gap-2.5">
                             <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=14" alt="Jese image">
                             <div class="flex flex-col gap-1">
-                                <div class="flex flex-col w-full max-w-[326px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                                <div class="flex flex-col w-full max-w-[326px] leading-1.5 p-4 border-gray-200 bg-gray-300  rounded-e-xl rounded-es-xl dark:bg-gray-700">
                                 <div class="flex items-center space-x-2 rtl:space-x-reverse mb-2">
-                                    <span class="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
+                                    <span class="text-sm font-semibold text-gray-600 dark:text-white">Bonnie Green</span>
                                     <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
                                 </div>
-                                <p class="text-sm font-normal text-gray-900 dark:text-white">This is the new office </p>
+                                <p class="text-sm font-normal text-gray-600 dark:text-white">This is the new office </p>
                                 <div class="group relative my-2.5">
                                     
                                     <img src="https://flowbite.com/docs/images/blog/image-2.jpg" class="rounded-lg" />
@@ -188,12 +193,12 @@
                         <div class="flex items-start gap-2.5">
                             <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=14" alt="Jese image">
                         <div class="flex flex-col gap-1">
-                            <div class="flex flex-col w-full max-w-[326px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                            <div class="flex flex-col w-full max-w-[326px] leading-1.5 p-4 border-gray-200 bg-gray-300 rounded-e-xl rounded-es-xl dark:bg-gray-700">
                                 <div class="flex items-center space-x-2 rtl:space-x-reverse mb-2">
-                                    <span class="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
+                                    <span class="text-sm font-semibold text-gray-600 dark:text-white">Bonnie Green</span>
                                     <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
                                 </div>
-                                <p class="text-sm font-normal text-gray-900 dark:text-white">This is the new office </p>
+                                <p class="text-sm font-normal text-gray-600 dark:text-white">This is the new office </p>
                                 <div class="grid gap-4 grid-cols-2 my-2.5">
                                     <div class="group relative">
                                         
