bb4315b9bc794fd485135861b09999e0c2ade00b
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-12-29 17:21:12 +0100

    fixes

--- components/Login.vue
+++ components/Login.vue
@@ -38,7 +38,7 @@
                         </div>
                          <!-- <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a> -->
                     </div>
-                    <button @click.prevent="login" type="submit" class="w-full text-white bg-primary-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+                    <button @click.prevent="login" type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                   
                 </form>
             </div>
