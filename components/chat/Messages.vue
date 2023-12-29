cdaa446521392e200d5d0e3e48c33cd5252e6979
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-12-29 18:10:41 +0100

    fixes

--- components/chat/Messages.vue
+++ components/chat/Messages.vue
@@ -120,6 +120,75 @@
                                 </div>
                         </div>     
                      </div>
+
+
+                    <div class="chat-message">
+                        <div class="flex items-end">
+
+                        <div class="flex items-start gap-2.5">
+                            <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=14" alt="Jese image">
+                        <div class="flex flex-col gap-1">
+                            <div class="flex flex-col w-full max-w-[326px] leading-1.5 p-4 border-gray-200 bg-gray-300 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                                <div class="flex items-center space-x-2 rtl:space-x-reverse mb-2">
+                                    <span class="text-sm font-semibold text-gray-600 dark:text-white">Bonnie Green</span>
+                                    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
+                                </div>
+                                <p class="text-sm font-normal text-gray-600 dark:text-white">This is the new office </p>
+                                <div class="grid gap-4 grid-cols-2 my-2.5">
+                                    <div class="group relative cursor-pointer">
+
+                                        <img src="https://flowbite.com/docs/images/blog/image-1.jpg" class="rounded-lg" />
+                                    </div>
+                                    <div class="group relative cursor-pointer">
+
+                                        <img src="https://flowbite.com/docs/images/blog/image-2.jpg" class="rounded-lg" />
+                                    </div>
+                                    <div class="group relative cursor-pointer">
+
+                                        <img src="https://flowbite.com/docs/images/blog/image-3.jpg" class="rounded-lg" />
+                                    </div>
+                                    <div class="group relative cursor-pointer">
+
+                                        <img src="https://flowbite.com/docs/images/blog/image-1.jpg" class="rounded-lg" />
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        </div>
+
+
+
+                        </div>
+                    </div>
+
+                    <div class="chat-message">
+                        <div class="flex items-end">
+
+                        <div class="flex items-start gap-2.5">
+                            <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=14" alt="Jese image">
+                        <div class="flex flex-col gap-1">
+                            <div class="flex flex-col w-full max-w-[326px] leading-1.5 p-4 border-gray-200 bg-gray-300 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                                <div class="flex items-center space-x-2 rtl:space-x-reverse mb-2">
+                                    <span class="text-sm font-semibold text-gray-600 dark:text-white">Bonnie Green</span>
+                                    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
+                                </div>
+                                <p class="text-sm font-normal text-gray-600 dark:text-white">This is the new office </p>
+                                    <div class="group relative cursor-pointer pt-4">
+
+                                        <img src="https://flowbite.com/docs/images/blog/image-1.jpg" class="rounded-lg" />
+                                    </div>
+
+                            </div>
+                        </div>
+
+                        </div>
+
+
+
+                        </div>
+                    </div>
         
                 
     </div>
